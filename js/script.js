e6da40f8f31af9a73031b7fbf7d7ba604f63ca2a
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2024-02-17 23:15:42 +0300

    add second bigslider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
     $('.catalog_slider').slick({
         dots: true,
-        slidesToShow: 4,
+        slidesToShow: 7,
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        asNavFor: ".catalog_sliderbig",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+    $('.catalog_sliderbig').slick({
+        arrows: false,
+        asNavFor: ".catalog_slider",
     });
 });
